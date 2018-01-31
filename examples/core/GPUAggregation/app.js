a1280f47707d0d309b70c08b5e042ae86c3cf8ac
Ravi Akkenapally <chandu@uber.com>
2018-01-31 12:13:35 -0800

    Add additive blending

--- examples/core/GPUAggregation/app.js
+++ examples/core/GPUAggregation/app.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-var, max-statements */
 
-import {AnimationLoop, Program, Buffer, Matrix4, setParameters} from 'luma.gl';
-
+import {AnimationLoop, Program, Buffer, GL, setParameters} from 'luma.gl';
+import assert from 'assert';
 /* global console */
 /* eslint-disable no-console */
 
@@ -9,6 +9,7 @@ const VERTEX_SHADER = `\
 attribute vec2 positions;
 uniform vec2 windowRect;
 void main(void) {
+  // Map each vertex from (0,0):windowRect -> (-1, -1):(1,1)
   vec2 pos = (positions * (2., 2.) / (windowRect)) - (1., 1.);
   gl_Position = vec4(pos, 1.0, 1.0);
 }
@@ -20,21 +21,27 @@ precision highp float;
 #endif
 
 void main(void) {
-  gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+  gl_FragColor = vec4(0.2, 0.2, 1.0, 1.0);
 }
 `;
 
 const animationLoop = new AnimationLoop({
   useDevicePixels: false,
-  onInitialize({gl, canvas, aspect}) {
+  onInitialize({gl, canvas, aspect, width, height}) {
 
-    console.log(`Canvas W: ${canvas.width} H: ${canvas.height}`);
+    console.log(`Canvas W: ${width} H: ${height}`);
 
     setParameters(gl, {
       clearColor: [0, 0, 0, 1],
       clearDepth: [1],
       depthTest: true,
-      depthFunc: gl.LEQUAL
+      depthFunc: gl.LEQUAL,
+
+      blend: true,
+      blendEquation: GL.FUNC_ADD,
+      blendFunc: [GL.ONE, GL.ONE]
+//      blendColor: [1.0, 1.0, 1.0, 1.0],
+
     });
 
     var program = new Program(gl, {
@@ -42,16 +49,18 @@ const animationLoop = new AnimationLoop({
       fs: FRAGMENT_SHADER
     });
 
-    const LINE_VERTS = [50, 10,   canvas.width, canvas.height]; // [-1, -1,   0.9, 0.9];
-    const TRIANGLE_VERTS = [0, 1, 0,  -1, -1, 0,  1, -1, 0]; // eslint-disable-line
-    const SQUARE_VERTS = [1, 1, 0,  -1, 1, 0,  1, -1, 0,  -1, -1, 0]; // eslint-disable-line
+    // const LINE_VERTS = [50, 10,   canvas.width, canvas.height]; // [-1, -1,   0.9, 0.9];
+    const points = getRandomPoints({
+      x: 0,
+      y: 0,
+      width: canvas.width,
+      height: canvas.height,
+      count: 100
+    });
+    const boundingRect = getBoundingRect(points, 100);
 
-    var linePositions = new Buffer(gl, {size: 2, data: new Float32Array(LINE_VERTS)});
-    var trianglePositions = new Buffer(gl, {size: 3, data: new Float32Array(TRIANGLE_VERTS)});
-    var squarePositions = new Buffer(gl, {size: 3, data: new Float32Array(SQUARE_VERTS)});
-
-    const view = new Matrix4(); // .translate([-1.5, 0, -7]);
-    const projection = new Matrix4(); // .perspective({aspect});
+    var linePositions = new Buffer(gl, {size: 2, data: new Float32Array(points)});
+    const boundingRectPositions = new Buffer(gl, {size: 2, data: new Float32Array(boundingRect)});
     const windowRect = [canvas.width, canvas.height];
     program.use();
 
@@ -65,33 +74,26 @@ const animationLoop = new AnimationLoop({
       .setUniforms({
         windowRect
       });
-    gl.drawArrays(gl.POINTS, 0, 2);
+    gl.drawArrays(gl.POINTS, 0, 100);
 
-/*
-    // Draw Triangle
+    // EACH draw render (0.2, 0.2, 1.0) as it looks blue ish here but following draw make it white.
+    gl.drawArrays(gl.POINTS, 0, 100);
+    gl.drawArrays(gl.POINTS, 0, 100);
+    gl.drawArrays(gl.POINTS, 0, 100);
+    gl.drawArrays(gl.POINTS, 0, 100);
+
     program
       .setBuffers({
-        positions: trianglePositions
+        positions: boundingRectPositions
       })
       .setUniforms({
-        uMVMatrix: view,
-        uPMatrix: projection
+        windowRect
       });
-    gl.drawArrays(gl.TRIANGLES, 0, 3);
-*/
-/*
-    // Draw Square
-//    view.translate([3, 0, 0]);
-    program
-      .setBuffers({
-        positions: squarePositions
-      })
-      .setUniforms({
-        uMVMatrix: view,
-        uPMatrix: projection
-      });
-    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
-*/
+    gl.drawArrays(gl.LINES, 0, 2);
+    gl.drawArrays(gl.LINES, 0, 2);
+    gl.drawArrays(gl.LINES, 0, 2);
+    gl.drawArrays(gl.LINES, 0, 2);
+
   }
 });
 
@@ -105,6 +107,48 @@ animationLoop.getInfo = () => {
     The classic WebGL Lessons in luma.gl
     `;
 };
+
+// cell start :origin (x, y)
+// cell size : rect (width, height)
+// number of points: count
+// Result count number of points in an array , each point (x, y)
+// such that  origin.x <= x < orgin.x + width
+function getRandomPoints(opts) {
+  assert(
+    Number.isFinite(opts.x) &&
+    Number.isFinite(opts.y) &&
+    Number.isFinite(opts.width) &&
+    Number.isFinite(opts.height) &&
+    Number.isFinite(opts.count)
+  );
+  const points = new Array(opts.count * 2);
+  for (let i = 0; i < opts.count; i++) {
+    points[i * 2] = Math.floor(Math.random() * opts.width) + opts.x;
+    points[(i * 2) + 1] = Math.floor(Math.random() * opts.height) + opts.y;
+  }
+  return points;
+}
+
+function getBoundingRect(points, count) {
+  const rect = [points[0], points[1], points[0], points[1]];
+  for (let i = 1; i < count; i++) {
+    const x = points[i * 2];
+    const y = points[(i * 2) + 1];
+    if (x < rect[0]) {
+      rect[0] = x;
+    }
+    if (y < rect[1]) {
+      rect[1] = y;
+    }
+    if (x > rect[2]) {
+      rect[2] = x;
+    }
+    if (y > rect[3]) {
+      rect[3] = y;
+    }
+  }
+  return rect;
+}
 
 export default animationLoop;
 
